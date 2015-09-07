12d897cf24f4439f2c9fa8850015b9db91705cab
gleb.zamyslovskiy <gleb.zamyslovskiy@wisercat.eu>
2015-09-07 23:16:02 +0300

    init

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 var server = require("./server");
 
-<<<<<<< HEAD
 server.start();
-=======
-server.start();
-
->>>>>>> f794a878647508d62f15a52e9cd9c43a66c34cea
--- server.js
+++ server.js
@@ -1,28 +1,10 @@
 var http = require("http");
-<<<<<<< HEAD
+
 var url = require("url");
 
 function start() {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
-=======
-
-function start() {
-  function onRequest(request, response) {
-    console.log("Request received.");
->>>>>>> f794a878647508d62f15a52e9cd9c43a66c34cea
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Worlds");
-    response.end();
-  }
-
-  http.createServer(onRequest).listen(8888);
-<<<<<<< HEAD
-  console.log("Server has started.");
-=======
-  console.log("Server has started, hello2.");
->>>>>>> f794a878647508d62f15a52e9cd9c43a66c34cea
-}
 
 exports.start = start;

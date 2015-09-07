7280c448554a21c256d4e361b279b2ebde98e3c7
Andrey Gaponik <andreygaponik@gmail.com>
2015-09-07 15:01:57 +0300

    first commits

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ function start() {
   }
 
   http.createServer(onRequest).listen(8888);
-  console.log("Server has started, hello.");
+  console.log("Server has started, hello2.");
 }
 
 exports.start = start;
